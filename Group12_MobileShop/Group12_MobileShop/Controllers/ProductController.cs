22570153a46230e8f76e5c66880caa64b0f2c5bc
hoanglhhe161738 <105199898+hoanglhhe161738@users.noreply.github.com>
2024-02-28 18:04:50 +0700

    Update screen sản phẩm

--- Group12_MobileShop/Group12_MobileShop/Controllers/ProductController.cs
+++ Group12_MobileShop/Group12_MobileShop/Controllers/ProductController.cs
@@ -27,5 +27,30 @@ namespace Group12_MobileShop.Controllers
             List<Product> list = JsonSerializer.Deserialize<List<Product>>(responseBody);
             return View(list);
         }
+
+        [HttpGet]
+        public async Task<IActionResult> filterPriceHighToLow()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(productUrl);
+
+            string responseBody = await responseMessage.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            List<Product> list = JsonSerializer.Deserialize<List<Product>>(responseBody);
+            List<Product> list2 = list.OrderByDescending(p => p.manufacturer).ToList();
+            
+            return View("Index",list2) ;
+        }
+
+        [HttpGet]      
+        public async Task<IActionResult> filterPriceLowToHigh()
+        {
+            HttpResponseMessage responseMessage = await client.GetAsync(productUrl);
+
+            string responseBody = await responseMessage.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            List<Product> list = JsonSerializer.Deserialize<List<Product>>(responseBody);
+            List<Product> list2 = list.OrderByDescending(p => p.manufacturer).ToList();
+            return View("Index", list2);
+        }
     }
 }
