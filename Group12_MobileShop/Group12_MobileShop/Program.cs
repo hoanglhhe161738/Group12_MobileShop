225be46663dea656a346d1db87ad53a172ec0954
Kasa <103961248+Kawoasa@users.noreply.github.com>
2024-03-24 00:26:47 +0700

    Update Program.cs

--- Group12_MobileShop/Group12_MobileShop/Program.cs
+++ Group12_MobileShop/Group12_MobileShop/Program.cs
@@ -1,8 +1,13 @@
+using Group12_MobileShop.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages();
+builder.Services.AddIdentity<AppUser, IdentityRole>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -13,11 +18,16 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapRazorPages();
+});
+IdentityUser user;
+IdentityDbContext context;
 app.Run();
