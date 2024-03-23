56b3892a0f27c4a82bb64901afdd9ec1b7ea5af8
HoangTV342002 <146700581+HoangTV342002@users.noreply.github.com>
2024-03-24 00:49:15 +0700

    created detail

---
+++ Group12_MobileShop/Group12_MobileShop/Views/Product/Detail.cshtml.cs
@@ -0,0 +1,22 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Group12_MobileShop.Views.Product
+{
+    public class DetailModel : PageModel
+    {
+        public string ModelName { get; set; }
+        public string Manufacturer { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public bool InStock { get; set; }
+        public void OnGet(string modelName, string manufacturer, decimal price, string description, bool inStock)
+        {
+            ModelName = modelName;
+            Manufacturer = manufacturer;
+            Price = price;
+            Description = description;
+            InStock = inStock;
+        }
+    }
+}
