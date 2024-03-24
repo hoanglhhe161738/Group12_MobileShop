3b6701e185be8704eaa8adcda180d29de13ca636
Kasa <103961248+Kawoasa@users.noreply.github.com>
2024-03-24 22:22:20 +0700

    Update Register.cshtml.cs

--- Group12_MobileShop/Group12_MobileShop/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Group12_MobileShop/Group12_MobileShop/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -109,9 +109,8 @@ namespace Group12_MobileShop.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
-        {
-            returnUrl ??= Url.Content("~/");
+        public async Task<IActionResult> OnPostAsync()
+        {           
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
